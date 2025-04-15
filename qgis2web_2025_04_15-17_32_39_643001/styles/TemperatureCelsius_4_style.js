63a5c9947b03581ebc97630b475a07fdd39fa26b
EKMorris-web <eleanormorris93@gmail.com>
2025-04-15 17:34:55 +0100

    Add files via upload

---
+++ qgis2web_2025_04_15-17_32_39_643001/styles/TemperatureCelsius_4_style.js
@@ -0,0 +1,61 @@
+var size = 0;
+var placement = 'point';
+
+var style_TemperatureCelsius_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("LST");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 37.800000 && value <= 42.000000) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(215,25,28,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 33.600000 && value <= 37.800000) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(253,174,97,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 29.400000 && value <= 33.600000) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(255,255,191,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 25.200000 && value <= 29.400000) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(171,221,164,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 21.000000 && value <= 25.200000) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(43,131,186,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
